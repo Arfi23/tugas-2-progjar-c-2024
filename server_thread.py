9469ba3c972e2f87b57bfae9162091847bdfa430
Arfi23 <arfifaizilraushani@gmail.com>
2024-06-03 09:24:19 +0700

    Create Server class

--- server_thread.py
+++ server_thread.py
@@ -20,3 +20,27 @@ class ProcessTheClient(threading.Thread):
                 break
         self.connection.close()
 
+class Server(threading.Thread):
+    def __init__(self) :
+        self.the_clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        threading.Thread.__init__(self)
+    
+    def run(self):
+        self.my_socket.bind(('0.0.0.0', 45000))
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning(f"connection from {self.client_address}")
+
+            clt = ProcessTheClient(self.connection, self.client_address)
+            clt.start()
+            self.the_clients.append(clt)
+
+
+def main():
+    svr = Server()
+    svr.start()
+
+if __name__ == "__main__":
+    main()
