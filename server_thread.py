c006e4af0d773394fba6103d29604ea07d988602
Arfi23 <arfifaizilraushani@gmail.com>
2024-06-03 10:59:25 +0700

    Create the RequestCommand class

--- server_thread.py
+++ server_thread.py
@@ -2,38 +2,72 @@ from socket import *
 import socket
 import threading
 import logging
-import time
+from time import gmtime, strftime
 import sys
+
+class RequestCommand:
+    @staticmethod
+    def time_cmd(connection):
+        message = f"JAM {strftime('%H:%M:%S', gmtime())}\r\n"
+        connection.sendall(message.encode("utf-8"))
+
+    @staticmethod
+    def quit_cmd(connection):
+        message = "QUIT request diterima\r\n"
+        connection.sendall(message.encode("utf-8"))
+        connection.close()
+    
+    @staticmethod
+    def unknown_cmd(connection):
+        message = "WARNING: perintah tidak dikenali"
+        connection.sendall(message.encode("utf-8"))
 
 class ProcessTheClient(threading.Thread):
     def __init__(self, connection, address):
+        super().__init__()
         self.connection = connection
         self.address = address
-        threading.Thread.__init__(self)
 
     def run(self):
-        while True:
-            data = self.connection.recv(1024)
-            if data:
-                self.connection.sendall(data)
-            else:
-                break
-        self.connection.close()
+            while True:
+                try:
+                    data = self.connection.recv(1024)
+                    if data:
+                        command = data.decode('utf-8').strip()
+                        logging.warning(f"Data diterima: {command} dari client {self.address}.")
+                        if command == 'TIME\r\n':
+                            logging.warning(f"Request berupa perintah TIME dari client {self.address}.")
+                            RequestCommand.time_cmd(self.connection)
+                        elif command == 'QUIT\r\n':
+                            logging.warning(f"Requst berupa perintah QUIT dari client {self.address}.")
+                            RequestCommand.quit_cmd(self.connection)
+                            break
+                        else:
+                            logging.warning(f"Request {command} dari client {self.address} tidak dikenali.")
+                            RequestCommand.unknown_cmd(self.connection)
+                    else:
+                        break
+                except OSError:
+                    break
+            self.connection.close()
 
 class Server(threading.Thread):
     def __init__(self) :
+        super().__init__()
         self.the_clients = []
         self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        threading.Thread.__init__(self)
+        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
     def run(self):
         self.my_socket.bind(('0.0.0.0', 45000))
         self.my_socket.listen(1)
-        while True:
-            self.connection, self.client_address = self.my_socket.accept()
-            logging.warning(f"connection from {self.client_address}")
+        logging.warning(f"Server listening on {self.host}:{self.port}")
 
-            clt = ProcessTheClient(self.connection, self.client_address)
+        while True:
+            conn, cli_address = self.my_socket.accept()
+            logging.warning(f"connection from {cli_address}")
+
+            clt = ProcessTheClient(conn, cli_address)
             clt.start()
             self.the_clients.append(clt)
 
